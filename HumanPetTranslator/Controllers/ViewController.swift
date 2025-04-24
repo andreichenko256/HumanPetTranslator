7a3a46645891ccbefd3dd7c90073d1aaa2bef301
andreichenko256 <andreichenko.kirill2000@gmail.com>
2025-04-24 16:08:36 +0300

    Add base view and constants utility

--- HumanPetTranslator/Controllers/ViewController.swift
+++ HumanPetTranslator/Controllers/ViewController.swift
@@ -1,11 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
-
 }
 
